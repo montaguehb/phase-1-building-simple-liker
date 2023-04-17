318254a790ce56b166fc80843d014cc179e4a1db
Historia Montague <historia.montague@outlook.com>
2023-04-17 11:53:43 -0600

    Finished Lab

--- main.js
+++ main.js
@@ -3,10 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  for (const likeButton of document.querySelectorAll(".like-glyph"))
+  {  
+    likeButton.addEventListener("click", event => {
+      mimicServerCall()
+      .then( () => {
+        if(event.target.textContent === EMPTY_HEART) {
+          event.target.textContent = FULL_HEART;
+          event.target.classList.add("activated-heart");
+        }
+        else {
+          event.target.textContent = EMPTY_HEART;
+          event.target.classList.remove("activated-heart");
+        }
+      })
+      .catch((error) => {
+        modal.classList.remove("hidden")
+        modal.textContent = error;
+        setTimeout(() => modal.classList.add("hidden"), 3000);
+      });
+    });
+  }
+})
 
 
-
-
+modal.classList.add("hidden");5
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
